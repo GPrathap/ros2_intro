da1aa60fe8f71d1e902a741a5d0a5c6a4324e418
GPrathap <ggeesara@gmail.com>
2023-06-08 08:19:28 +0300

    adding lecture 02

--- hello_world/launch/pub_sub_variant_2.py
+++ hello_world/launch/pub_sub_variant_2.py
@@ -6,6 +6,24 @@ def generate_launch_description():
 
     # TODO First create a LaunchDescription() and then add 
     # publisher_node and subscriber_node 
-    # Finally, return the LaunchDescription you created 
+    # Finally, return the LaunchDescription you created
+    
+    pub_cmd = Node(
+        package='hello_world',
+        executable='publisher_node',
+        output='screen'
+    )
+    
+    sub_cmd = Node(
+        package='hello_world',
+        executable='subscriber_node',
+        output='screen'
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(pub_cmd)
+    ld.add_action(sub_cmd)
+
+    return ld 
     
     return 
---
+++ hello_world/src/friend_hello_pub_class.cpp
@@ -0,0 +1,35 @@
+#include <rclcpp/rclcpp.hpp>
+#include <friend_msgs/msg/friend_info.hpp>
+
+
+using namespace std::chrono_literals;
+class SenderNode : public rclcpp::Node{
+  public:
+  
+    SenderNode() : Node("SenderNode"){
+      publisher_ = create_publisher<friend_msgs::msg::FriendInfo>("/r2d2_pose", 10);
+      timer_ = create_wall_timer(500ms, std::bind(&SenderNode::timer_callback, this));
+    }
+
+    void timer_callback(){
+      message_.id = 345;
+      message_.name = "d2r2";
+      publisher_->publish(message_);
+    }
+  
+  private:
+    rclcpp::Publisher<friend_msgs::msg::FriendInfo>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    friend_msgs::msg::FriendInfo message_;
+};
+
+int main(int argc, char** argv){
+  rclcpp::init(argc, argv);
+
+  auto node = std::make_shared<SenderNode>();
+  
+  rclcpp::spin(node);
+
+  rclcpp::shutdown();
+  return 0;
+}
