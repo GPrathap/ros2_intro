57b08a51d09c79ca20a91b43bf0aa9a994a7662a
GPrathap <ggeesara@gmail.com>
2025-07-02 16:34:56 +0100

    update sdf file

--- hagen_robot/hagen_robot_diff_drive/hagen_robot_diff_drive/spawn_robot.py
+++ hagen_robot/hagen_robot_diff_drive/hagen_robot_diff_drive/spawn_robot.py
@@ -13,7 +13,7 @@ def main():
     rclpy.init()
     sdf_file_path = os.path.join(
         get_package_share_directory("hagen_robot_diff_drive"), "models",
-        "hagen_robot_model", "hagen_robot_model.sdf")
+        "hagen_robot_model", "hagen_robot_model_v0.sdf")
     
     
     node = rclpy.create_node("entity_spawner")
